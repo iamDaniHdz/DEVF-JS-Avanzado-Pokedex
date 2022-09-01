0ace8946dec96b5b8fb5769bf3888c06e24323c7
Daniel Hernandez <daniel244@live.com.mx>
2022-08-31 20:13:50 -0500

    add main.js, poster.js

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+const URL = "https://pokeapi.co/api/v2";
+const API_KEY = "e73f90faf9a2f65e8bb82591f82eea41";
+
+// colores a usar segun el tipo de pokemon
+const typeColors = {
+    electric: '#FFE033',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFADC9',
+    ghost: '#561D25',
+    bug: '#77F879',
+    poison: '#795663',
+    ground: '#C39546',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fairy: '#FA75D2',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+    white: '#FFF',
+};
+
+const typeColorsOpacity = {
+    electric: '#FFED85',
+    normal: '#ccb8bb',
+    fire: '#ffa099',
+    water: '#10bff9',
+    ice: '#c5f0fc',
+    rock: '#cdcbcd',
+    flying: '#baf2e1',
+    grass: '#76bca8',
+    psychic: '#FFEBF1',
+    ghost: '#B83D4E',
+    bug: '#B1FBB3',
+    poison: '#AA8894',
+    ground: '#D7B884',
+    dragon: '#E89CAC',
+    steel: '#54CEDE',
+    fairy: '#fdb9e9',
+    fighting: '#666666',
+    default: '#653E4A',
+    white: '#FFF',
+};
